2eb8053b1414a7c442e58b270b3b1a79797c3e91
Rash <rash@tikiti.com>
2025-09-01 11:12:27 +0000

    Remove public email config file to fix security issue - use environment variables instead

--- public/email-config.js
+++
@@ -1,6 +0,0 @@
-// Email configuration for static HTML files
-window.EMAIL_CONFIG = {
-  sendgridApiKey: 'YOUR_SENDGRID_API_KEY_HERE',
-  fromEmail: 'gettikiti@gmail.com',
-  fromName: 'Tikiti Events'
-};
